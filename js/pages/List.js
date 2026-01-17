302de75b664fac5a3f39651fcdb3b9e148e4c1c8
Darkpraire <darkpraire@gmail.com>
2026-01-16 23:52:43 -0800

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -104,22 +104,19 @@ export default {
                     </template>
                     <h3>Submission Requirements</h3>
                     <p>
-                        Achieved the record without using hacks (however, FPS bypass is allowed, up to 360fps)
+                        Achieved the record without using hacks
                     </p>
                     <p>
                         Achieved the record on the level that is listed on the site - please check the level ID before you submit a record
                     </p>
                     <p>
-                        Have either source audio or clicks/taps in the video. Edited audio only does not count
+                        The recording must have a previous attempt and entire death animation shown before the completion, unless the completion is on the first attempt. The recording must also show the player hit the endwall, or the completion will be invalidated.
                     </p>
                     <p>
-                        The recording must have a previous attempt and entire death animation shown before the completion, unless the completion is on the first attempt.
+                        The recording needs to be in a good quality, handcam if possible, loud and clear clicks
                     </p>
                     <p>
-                        The recording must also show the player hit the endwall, or the completion will be invalidated.
-                    </p>
-                    <p>
-                        Do not use secret routes or bug routes
+                        Mods that show the click sounds in game are not allowed
                     </p>
                     <p>
                         CBF(Click Between Frames) is allowed
